4a7f4d0730c1f8d22e3e82d292ea67d4782bdfe4
jcezart <jcezar.t@gmail.com>
2024-10-30 08:07:40 -0300

    Adição da DP

--- app/src/main/java/com/example/fuelcalculator/MainActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/MainActivity.kt
@@ -6,15 +6,27 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -48,17 +60,44 @@ fun ImagemFundo(){
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ConteudoApp() {
-    Box(modifier = Modifier.fillMaxSize()) {
+    var distancia by remember { mutableStateOf("")}
+    val customColor = Color(0xFF013220)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
             text = "Fuel Calculator",
             color = Color(0xFF013220),
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
             modifier = Modifier
-                .align(Alignment.TopCenter)
                 .padding(top = 64.dp)
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        OutlinedTextField(
+            value = distancia,
+            onValueChange = {distancia = it},
+            label = { Text("Distância Percorrida (km)")},
+            placeholder = { Text("Digite a distância percorrida em km")},
+            modifier = Modifier.fillMaxWidth(),
+            textStyle = TextStyle(color = customColor),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedPlaceholderColor = Color.DarkGray,
+                focusedLabelColor = customColor,
+                unfocusedLabelColor = customColor,
+                focusedBorderColor = customColor,
+                unfocusedBorderColor = customColor,
+                cursorColor = customColor
+            )
         )
     }
 }
